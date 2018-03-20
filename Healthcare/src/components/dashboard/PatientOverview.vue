6ce346f7065446fdb4cfdefaec8796c1c62b37f0
Wasser,Jeroen J.J.M <j.wasser@student.fontys.nl>
2018-03-20 14:45:58 +0100

    Patient Overview done

--- Healthcare/src/components/dashboard/PatientOverview.vue
+++ Healthcare/src/components/dashboard/PatientOverview.vue
@@ -10,7 +10,7 @@
                   <tr>
                     <th class="thoverview">Voornaam</th>
                     <th class="thoverview">Achternaam</th>
-                    <th class="thoverview">Geboortedatum</th>
+                    <th class="thoverview">Leeftijd</th>
                     <th class="thoverview">Geslacht</th>
                     <th class="thoverview">Acties</th>
                   </tr>
@@ -21,9 +21,9 @@
                 <table class="tableoverview" cellpadding="0" cellspacing="0" border="0">
                   <tbody id="form-list-client-body">
                     <tr v-for="patient in patients">
-                      <td class="tdoverview"><input class="form-control" v-model="patient.firstName" readonly/></td>
-                      <td class="tdoverview"><input class="form-control" v-model="patient.lastName" readonly/></td>
-                      <td class="tdoverview"><input class="form-control" v-model="patient.birthDate" readonly/></td>
+                      <td class="tdoverview"><input class="form-control" v-model="patient.firstname" readonly/></td>
+                      <td class="tdoverview"><input class="form-control" v-model="patient.surname" readonly/></td>
+                      <td class="tdoverview"><input class="form-control" v-model="patient.age" readonly/></td>
                       <td class="tdoverview"><input class="form-control" v-model="patient.gender" readonly/></td>
                       <td class="tdoverview">
                         <a title="view this user" class="btn btn-default btn-sm "> <i style="font-size:24px" class="fa">&#xf06e;</i> </a>
@@ -45,25 +45,14 @@ export default{
   name: 'patientoverview',
   data() {
     return {
-      patients: [
-      {firstName: "Eduardo", lastName: "Gonzales", birthDate: "01-02-1990", gender: "M"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      {firstName: "Susan", lastName: "Gonzales", birthDate: "19-07-1991", gender: "F"},
-      ],
+      patients: [],
     }
+  },
+  mounted() {
+    this.$store.dispatch("getRequest", 'patients').then((response) => {
+      this.patients = response
+      console.log(this.patients)
+    });
   },
   methods: {
   addRow: function (voornaam, achternaam, geboortedatum, geslacht) {
@@ -73,7 +62,7 @@ export default{
            {
                console.log(e);
            }
-       }
+       },
   }
 }
 
