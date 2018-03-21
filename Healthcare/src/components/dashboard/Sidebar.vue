d1f155114ff1e21def0b26da72201eaf34388360
Devin Pennings <devinpennings@gmail.com>
2018-03-21 10:07:34 +0100

    Fixed loading dossier

--- Healthcare/src/components/dashboard/Sidebar.vue
+++ Healthcare/src/components/dashboard/Sidebar.vue
@@ -8,22 +8,26 @@
 
         <div class="profile-userpic">
           <img src="https://studiomango.nl/wp-content/uploads/2014/10/team-profile-picture_minko.jpg" class="img-responsive" alt="">
+          <h6> {{ user.firstname + ' ' + user.lastname }} </h6>
         </div>
 
           <li><a v-on:click="changeCompontent('home')" style="cursor:pointer"> <i class="ion-home"></i>Home</a></li>
 
-          <div v-if="userType === 'admin'">
-            <li><a style="cursor:pointer"> <i class="ion-ios-people"></i>Werknemers</a></li>
-            <li><a style="cursor:pointer"> <i class="ion-person"></i>Patienten</a></li>
+          <div v-if="user.type === 'admin'">
+            <li><a style="cursor:pointer"><i class="ion-ios-people"></i>Werknemers</a></li>
+            <li><a style="cursor:pointer"><i class="ion-person"></i>Patienten</a></li>
           </div>
 
-          <div v-if="userType === 'arts'">
-            <li><a style="cursor:pointer"> <i class="ion-person"></i>Patienten</a></li>
+          <div v-if="user.type === 'arts'">
+            <li><a style="cursor:pointer"><i class="ion-person"></i>Patienten</a></li>
           </div>
 
-          <div v-if="userType === 'patient'">
-            <li><a v-on:click="changeCompontent('personalDossier')" style="cursor:pointer"> <i class="ion-document"></i>Mijn dossier</a></li>
+          <div v-if="user.type === 'patient'">
+            <li><a v-on:click="changeCompontent('personalDossier')" style="cursor:pointer"><i class="ion-document"></i>Mijn dossier</a></li>
           </div>
+
+          <li><a v-on:click="logout" style="cursor:pointer"><i class="ion-android-exit"></i>Logout</a></li>
+
         </ul>
         <div class="sidenav-header d-flex align-items-center justify-content-center">
           <img src="../../assets/img/logoZonnevelt-02.svg">
@@ -38,13 +42,18 @@
     name:'navbar',
     data(){
       return{
-        userType: this.$store.getters.user.type,
+        user: this.$store.getters.user,
         cPersonalDossier: false,
       }
     },
     methods: {
       changeCompontent (component) {
         this.$parent.changeComponent(component)
+      },
+      logout(){
+        this.$store.dispatch("logout").then(() => {
+          this.$router.push('/login');
+        });
       }
     }
   }
