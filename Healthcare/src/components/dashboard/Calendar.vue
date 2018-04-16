de40a902ee27f252302db2a1558a89ad6583f47f
Ottons <ottonaus@gmail.com>
2018-04-16 10:43:37 +0200

    Calendar added

--- Healthcare/src/components/dashboard/Calendar.vue
+++ Healthcare/src/components/dashboard/Calendar.vue
@@ -2,20 +2,30 @@
   <div class="row times">
 
     <div class="col-xs-4 text-center row times" v-for="day in takeDaysFromAppointments(appointments)">
-
-      <h2>{{ day.startTime }}</h2>
-
-      <div class="time-slot" v-for="appointment in takeAppointmentsForDay(day)">
-
-        <label> {{appointment.startTime}} <br>
-          <input type="checkbox" :id="appointments.id" v-model="appointmentcheckbox[day.id]">
-          <span v-if="!appointment.available">Booked</span>
-          <span v-if="appointment.available">Available</span>
+      <div class="day-slot">
+        <label>{{day.startTime.toString().substring(0,12)}} <br>
+          <input type="radio" v-model="selectedDay" :value="day" v-bind:id="day.id">
+          <p>Beschikbaar: {{takeAppointmentsForDay(day).length}}</p>
         </label>
-
+      </div>
+    </div>
+      <br>
+      <div v-if="selectedDay">
+      <div class="row">
+      <div v-bind:class="{ 'time-slot': appointment.available == true, 'time-slot-unavailable': appointment.available == false }" v-for="appointment in takeAppointmentsForDay(selectedDay)">
+        <label> {{appointment.startTime}} <br>
+          <input type="checkbox" v-bind:id="appointment.id" v-model="appointment.available" v-on:input="appointment.available = $event.target.value">
+          <span v-if="!appointment.available">Bezet</span>
+          <span v-if="appointment.available">Beschikbaar</span>
+        </label>
+      </div>
+      </div>
+        <div class="row">
+          <textarea class="textarea" placeholder="De reden van mijn bezoek is: " v-model="note"></textarea>
+        </div>
       </div>
 
-    </div>
+
 
   </div>
 </template>
@@ -26,9 +36,12 @@
       data() {
         return {
           appointmentcheckbox: "",
+          selectedDay: "",
+          note: "",
           doctor: "",
           patient: "",
           props: ['doctorid'],
+          checkboxVal: "hoi",
           appointments: [],
           fields: {
             startTime: {label: 'Dag', sortable: true},
@@ -41,24 +54,51 @@
       created () {
           this.$store.dispatch("getRequest", 'timeslots').then((response) => {
           this.appointments = this.ConvertToDatetime(response);
-          console.log(this.appointments)
+            console.log("Eind: ");
+            console.log(this.appointments)
         });
       },
       methods: {
+        update(selectedAppointment) {
+          this.$store.dispatch('postRequest', {
+            url:'timeslot',
+            body:{
+              doctor: this.name,
+              patient: this.lname,
+              appointment: selectedAppointment,
+              note: this.note,
+            }
+          }).then(() => {
+            this.changeComponent('viewWerknemers')
+          });
+        },
         takeAppointmentsForDay(day) {
           var index = this.appointments.map(function(x) {return x.id; }).indexOf(day.id);
+          var date = day.startTime.toString().substring(0,12);
+          var compareDate;
           var dailyAppointments = [];
-            for (var i = index; i < index + 24; i++) {
-              dailyAppointments.push(this.appointments[i]);
+            for (var i = index; i < this.appointments.length; i++) {
+              compareDate = this.appointments[i].startTime.toString().substring(0,12);
+              if(compareDate == date){
+                dailyAppointments.push(this.appointments[i]);
+              }
             }
-            console.log(dailyAppointments)
+            console.log("daily: ");
+            console.log(dailyAppointments);
             return dailyAppointments;
         },
         takeDaysFromAppointments(dataValues) {
           var days = [];
-          for (var i = 0; i < 480; i = i + 24){
-            days.push(dataValues[i])
+          var currentTimeSlot = "";
+          var newTimeSlot = "";
+          for (var i = 0; i < dataValues.length; i++){
+            newTimeSlot = dataValues[i].startTime.toString().substring(0,12);
+            if(currentTimeSlot != newTimeSlot){
+              days.push(dataValues[i]);
+              currentTimeSlot = newTimeSlot;
+            }
           }
+          console.log(days);
           return days;
         },
         ConvertToDatetime(dateValues) {
